3bbedbb26a861e040a8dd80fcad4961717207851
0xkmr_ <Mkumarkokkiligadda@gmail.com>
2023-07-25 11:21:05 +0530

    teting

---
+++ src/imports.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "lib/solmate/src/tokens/ERC20.sol";
+
+contract Token is ERC20{
+
+    constructor() ERC20("name","symbol",18){
+        
+    }
+}
